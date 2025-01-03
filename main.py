e313f0d73bb8b0e1824736131673f64e23ab812a
doublefree-fan <fpgr07@gmail.com>
2025-01-03 01:52:41 +0000

    add count_words

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from book_paths import FRANKENSTEIN_PATH_FILE
 
+def count_words(book):
+    """
+    Counts the number of words in a given book.
+    Args:
+        book (str): the text of the book
+    Returns:
+        int: the number of words in the book
+    Raises:
+        Exception: if the book is not provided (empty string or None)
+    """
+    if not book:
+        raise Exception("book was not provided")
+    return len(book.split())
+
 def read_book(book_path):
     """
-    Reads contents of a book file from the given the path.
+    Reads contents of a book file from the given the path
     Args:
         book_path: string path to the book file
     Returns:
-        string containing the entire book contents
+        string: containing the entire book contents
     Raises:
         Exception: if book_path is empty
     """
@@ -19,7 +33,8 @@ def read_book(book_path):
 def main():
     try:
         book = read_book(FRANKENSTEIN_PATH_FILE)
-        print(book)
+        book_words = count_words(book)
+        print(book_words)
     except Exception as e:
         print(e)
 
